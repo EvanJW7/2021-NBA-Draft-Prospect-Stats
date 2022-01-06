72fe6f152b415ccfff8fead23e184ad015d6e5dc
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-01-06 11:39:28 -0600

    Update 2021prospects.py

--- 2021prospects.py
+++ 2021prospects.py
@@ -5,24 +5,27 @@ headers = {'User Agent':'Mozilla/5.0'}
 
 players = [
     'cade-cunningham', 'jalen-suggs','evan-mobley', 'franz-wagner', 'davion-mitchell', 
-    'scottie-barnes','moses-moody', 'james-bouknight', 'ziaire-williams', 'corey-kispert', 'tre-mann', 'ayo-dosunmu',
+    'scottie-barnes','moses-moody', 'ziaire-williams', 'corey-kispert', 'tre-mann', 'ayo-dosunmu',
     'max-abmas', 'kai-jones', 'josh-christopher', 'nahshon-hyland', 'jaden-springer', 'cameron-thomas', 'miles-mcbride', 
-    'johnny-juzang', 'charles-bassey', 'chris-duarte', 'marcus-bagley', 'jeremiah-robinson-earl', 'sharife-cooper', 
-    'dj-stewart', 'brandon-bostonjr', 'joel-ayayi', 'isaiah-livers', 'david-duke', 'dayron-sharpe',
+    'johnny-juzang', 'charles-bassey', 'chris-duarte', 'james-bouknight', 'jeremiah-robinson-earl', 'sharife-cooper', 
+    'dj-stewart', 'brandon-bostonjr', 'joel-ayayi', 'isaiah-livers', 'david-duke', 'dayron-sharpe', 'keon-johnson',
     'marcus-zegarowski', 'rj-nembhard', 'aaron-henry', 'mj-walker', 'jericho-sims', 'raiquan-gray','feron-hunt', 
     'yves-pons', 'matthew-hurt', 'trey-murphyiii', 'ochai-agbaji', 'joshua-primo', 'aaron-wiggins', 'olivier-sarr', 
     'kessler-edwards', 'neemias-queta', 'jason-preston', 'sandro-mamukelashvili', 'jt-thor', 'quentin-grimes',
     'sam-hauser', 'matthew-mayer', 'macio-teague', 'javonte-smart', 'dj-steward', 'luka-garza', 'matthew-mcclung', 
     'aamir-simms', 'trendon-watford', 'justin-champagnie', 'austin-reaves', 'makur-maker', 'julian-champagnie',
-    'keve-aluma','dejon-jarreau', 'ej-liddell', 'jaquori-mclaughlin', 'derek-culver', 'moussa-cisse', 'michael-devoe',
-    'mckinley-wrightiv']
+    'keve-aluma','dejon-jarreau', 'jaquori-mclaughlin', 'derek-culver', 'moussa-cisse', 'michael-devoe',
+    'mckinley-wrightiv', 'isaiah-jackson-3', 'jalen-johnson-24']
 
 player_stats = []
 playerlist = []
 
 for player in players:
     try:
-        url = (f'https://www.sports-reference.com/cbb/players/{player}-1.html')
+        if player[-1].isdigit():
+            url = f'https://www.sports-reference.com/cbb/players/{player}.html'
+        else:
+            url = f'https://www.sports-reference.com/cbb/players/{player}-1.html'
         res = requests.get(url)
         soup = BeautifulSoup(res.content, 'lxml')
         header = [th.getText() for th in soup.findAll('tr', limit = 2)[0].findAll('th')]
@@ -30,10 +33,13 @@ for player in players:
         player_stats.append([td.getText() for td in soup.find('tr', id ='players_per_game.2021')])
         table = pd.DataFrame(player_stats, columns = header)
         player = player.replace('-',' ').title()
+        for char in player:
+            if char.isdigit():
+                player = player.replace(char, '')
         playerlist.append(player)
         
     except:
-        continue 
+        continue
      
 table.insert(0, "Name", playerlist)
 print(table)
