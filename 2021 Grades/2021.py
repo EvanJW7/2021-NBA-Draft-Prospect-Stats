9851616ef52208497d3a78fe698097a831460625
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2022-02-06 15:18:28 -0600

    Add files via upload

--- 2021 Grades/2021.py
+++ 2021 Grades/2021.py
@@ -5,17 +5,15 @@ import pandas as pd
 headers = {'User Agent':'Mozilla/5.0'}
 
 players = [
-    'cade-cunningham', 'jalen-suggs','evan-mobley', 'franz-wagner', 'davion-mitchell', 
-    'scottie-barnes','moses-moody', 'ziaire-williams', 'corey-kispert', 'tre-mann', 'ayo-dosunmu',
-    'kai-jones', 'josh-christopher', 'nahshon-hyland', 'jalen-johnson-24', 'jaden-springer', 'cameron-thomas', 
-    'miles-mcbride', 'johnny-juzang', 'charles-bassey', 'chris-duarte', 'james-bouknight', 'jeremiah-robinson-earl', 
-    'sharife-cooper', 'brandon-bostonjr', 'joel-ayayi', 'isaiah-livers', 'david-duke', 'dayron-sharpe', 
-    'keon-johnson', 'marcus-zegarowski', 'rj-nembhard', 'aaron-henry', 'feron-hunt', 
-    'matthew-hurt', 'trey-murphyiii', 'ochai-agbaji', 'joshua-primo', 'aaron-wiggins','kessler-edwards', 
-    'neemias-queta', 'jason-preston', 'sandro-mamukelashvili', 'jt-thor', 'quentin-grimes', 'joe-wieskamp',
-    'macio-teague', 'javonte-smart', 'dj-steward', 'luka-garza', 'matthew-mcclung', 'santi-aldama'
-    'trendon-watford', 'justin-champagnie', 'austin-reaves', 'keve-aluma', 'isaiah-jackson-3', 'jared-butler',
-    'greg-brown-9', 'dalano-banton', 'david-johnson-13', 'jericho-sims', 'raiquan-gray']
+    'isaiah-jackson-3', 'ayo-dosunmu', 'david-duke', 'sandro-mamukelashvili', 'corey-kispert', 'aaron-wiggins',
+    'matthew-mcclung', 'kai-jones',  'santi-aldama', 'cade-cunningham', 'jalen-suggs','evan-mobley', 'franz-wagner', 
+    'davion-mitchell', 'scottie-barnes','moses-moody', 'ziaire-williams', 'tre-mann', 'josh-christopher', 
+    'nahshon-hyland', 'jalen-johnson-24', 'jaden-springer', 'cameron-thomas', 'miles-mcbride', 'charles-bassey', 
+    'chris-duarte', 'james-bouknight', 'jeremiah-robinson-earl', 'sharife-cooper', 'brandon-bostonjr', 
+    'isaiah-livers','dayron-sharpe', 'keon-johnson', 'marcus-zegarowski', 'matthew-hurt', 'trey-murphyiii', 
+    'ochai-agbaji', 'joshua-primo', 'kessler-edwards', 'neemias-queta', 'jason-preston',  'jt-thor', 
+    'quentin-grimes', 'joe-wieskamp', 'javonte-smart', 'luka-garza', 'trendon-watford', 'austin-reaves', 
+    'greg-brown-9', 'dalano-banton', 'david-johnson-13', 'jericho-sims', 'raiquan-gray', 'jared-butler']
 
 player_stats = []
 playerlist = []
@@ -45,11 +43,14 @@ for player in players:
             player = player[:-2]
         if player[-2:] == 'jr':
             player = player.replace('jr', ' Jr')
+        if player[-3:] == 'iii':
+            player = player.replace('iii', ' III')
         playerlist.append(player)
         
     except:
         continue
     try:
+        player = player.replace('-', '+')
         url = f'https://www.google.com/search?q={player}+basketball+birth+date'
         res = requests.get(url)
         soup = BeautifulSoup(res.content, 'lxml')
@@ -84,13 +85,34 @@ for player in players:
         year_list.append(year)
         
     except:
-        age_list.append(22)
+        age_list.append(0)
         year_list.append(.85)
 
+age_list[0] = 19.5
+year_list[0] = 1.03
+age_list[1] = 21.4
+year_list[1] = .86
+age_list[2] = 21.7
+year_list[2] = .81
+age_list[3] = 22.1
+year_list[3] = .78
+age_list[4] = 22.3
+year_list[4] = .76
+age_list[5] = 22.5
+year_list[5] = .74
+age_list[6] = 22.5
+year_list[6] = .74
+age_list[7] = 20.4
+year_list[7] = .94
+age_list[8] = 20.5
+year_list[8] = .93
+age_list[-1] = 20.9
+year_list[-1] = .89
 
 table.insert(0, "Name", playerlist)
 table.insert(2, "Year", year_list)
 table.insert(2, "Age", age_list)
+
 table['MP'] = table['MP'].astype(float)
 table['PTS'] = table['PTS'].astype(float)
 table['AST'] = table['AST'].astype(float)
@@ -105,13 +127,11 @@ table['PF'] = table['PF'].astype(float)
 table['FT%'] = table['FT%'].astype(float)
 table['3P%'] = table['3P%'].astype(float)
 
-per40 = 40/table['MP']
-
-table["Player Grade"] = ((table['PTS']*per40) + (table['TRB']*1.5*per40) + (table['AST']*2*per40) +
-(table['BLK']*3*per40) + (table['STL']*3*per40) + (table['3P']*5*per40)+ (table['FT%']*7) + (table['SOS']) + 
+table["Player Grade"] = ((table['PTS']) + (table['TRB']*1.5) + (table['AST']*2) +
+(table['BLK']*3) + (table['STL']*3) + (table['3P']*5)+ (table['FT%']*7) + (table['SOS']) + 
 (table['3P%']*10)) * table['Year']
 
-table["Player Grade"] = table["Player Grade"]*1.25
+table["Player Grade"] = table["Player Grade"]*1.5
 table["Player Grade"] = (round(table["Player Grade"], 1))
 table["Player Grade"]= table["Player Grade"].astype(float)
                         
@@ -133,13 +153,10 @@ del table['2P']
 del table['2PA']
 del table['2P%']
 del table['Year']
-del table['Age']
+del table['TOV']
+del table['PF']
+
 print(table)
-
-
-
-
-
 
 #pd.DataFrame.to_excel(table, '/Users/evanwright/Downloads/Draft_Stats2021.xlsx')
 
